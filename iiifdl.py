ec00148a27818e179a431e3f1b8942075086e539
knagasaki <nagasaki@DESKTOP-4VPPBV9.localdomain>
2019-02-15 01:27:13 +0900

    Newly added

---
+++ iiifdl.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import urllib.request
+import json
+import sys
+
+url = sys.argv[1] #'http://www2.dhii.jp/nijl/NIJL0018/099-0014/manifest.json'
+readObj = urllib.request.urlopen(url)
+data = json.loads(readObj.read().decode('utf-8'))
+
+arImageUrls = []
+for seq in data['sequences']:
+     for canvas in seq['canvases']:
+         for image in canvas['images']:
+             arImageUrls.append(image['resource']['@id'])
+
+tLen = len(str(len(arImageUrls))) + 1
+fileNumber = 1
+print ('downloading...')
+for imageUrl in arImageUrls:
+    localFilename = str(fileNumber).zfill(tLen)+'.jpg'    
+    urllib.request.urlretrieve(imageUrl, localFilename)
+    fileNumber = fileNumber + 1
+    print (localFilename)
